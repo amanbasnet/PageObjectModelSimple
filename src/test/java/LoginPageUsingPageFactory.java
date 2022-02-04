50031129682e0f5e9352096d2bd343ce68620df1
AMAN BASNET <amanbasnet2000@gmail.com>
2022-02-04 15:57:41 +0530

    ok

---
+++ src/test/java/LoginPageUsingPageFactory.java
@@ -0,0 +1,24 @@
+import org.apache.xmlbeans.impl.xb.xsdschema.Public;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPageUsingPageFactory {
+    @FindBy(name = "email")
+    WebElement username;
+    @FindBy(name = "password")
+    WebElement pwd;
+    @FindBy(xpath = "//input[@value='Login']")
+    WebElement Loginbutton;
+    public void enterusername (String uname)
+    {
+        username.sendKeys(uname);
+    }
+    public void enterpassword (String password)
+    {
+        pwd.sendKeys(password);
+    }
+    public void Loginbutton()
+    {
+        Loginbutton.click();
+    }
+}
